edc3a0ccd293d2c7b19bd0e7f1afc4f32a05cbaa
kellysjohnson <KellyShimkoJohnson@Kellys-MacBook-Pro.local>
2015-04-19 20:04:06 -0500

    Some stuff, like Attack and pulling data from the DOM works

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-var Monster = { name: "", 
-				strength: "", 
-				dexterity: "", 
-				constitution: "", 
-				wisdom: "", 
-				intelligence: "", 
-				charisma: "" };
+var Monster = { name:"Hamburgler",
+				strength:8,
+				dexterity:7,
+				constitution:10,
+				wisdom:12,
+				intelligence:18,
+				charisma:15 }
 
-var Player = Object.create(Monster);
-Player.playerClass = "";
+console.log(Monster);
+
+var Hero = Object.create(Monster);
+Hero.playerClass = "Cleric";
+
+console.log(Hero);
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+/*
+var addCharacter = document.getElementById("addMonster");
+addCharacter.addEventListener("click", Player());
+
+console.log(Player);
+
+document.getElementById('addMonster').addEventListener('click', Player)
+*/
 
 
-
-function Player (name,strength,dexterity,constitution,wisdom,intelligence,charisma,playerclass) {
+function Player (name,strength,dexterity,constitution,wisdom,intelligence,charisma,playerClass) {
 	this.name = name;
 	this.strength = strength;
 	this.dexterity = dexterity;
@@ -19,30 +35,62 @@ function Player (name,strength,dexterity,constitution,wisdom,intelligence,charis
 	this.wisdom = wisdom;
 	this.intelligence = intelligence;
 	this.charisma = charisma;
-
-};
-
-playerArray = [];
-
-
-
-Monster.prototype = {
-	Attack: function () {
-		var role = randomNumber(1,20);
-		if (playerClass) {
+	this.playerClass = playerClass;
+	this.Attack = function () {
+		var role = randomNumber(1, 20);
+		if (playerClass == undefined) {
 			this.strength += role;
-			alert (characters.name + " has " + characters.strength + " for Strength \n" + characters.dexterity + "		for Dexterity \n");
+			alert(this.name + "s Charater Abilities are: \n" + this.strength + " for Strength \n" + this.dexterity + " for Dexterity \n" + this.constitution + " for Constitution\n" + this.wisdom + " for Wisdom\n" + this.intelligence + " for intelligence\n" + this.charisma + " for Charisma.");
 		} else {
+			alert("I have an ability!") 
 			this.dexterity += role;
 		} 
 	};
 }
 
-function randomNumber(min, max) {
-	return Math.floor(Math.random() * (1 + max - min) + min);
+document.getElementById("addMonster").addEventListener('click', addCharacter);
+
+function addCharacter(){
+	var nameInput = document.getElementById(name).value;
+	var strengthInput = document.getElementById(strength).value;
+	var dexterityInput = document.getElementById(dexterity).value;
+	var constitutionInput = document.getElementById(constitution).value;
+	var wisdomInput = document.getElementById(wisdom).value;
+	var intelligenceInput = document.getElementById(intelligence).value;
+	var charismaInput = document.getElementById(charisma).value;
+	var playerClassInput = document.getElementById(playerClass).value;
+}
+
+playerArray = [];
+
+if ()
+playerArray = playerArray.push()
 
 
-var Monster = 
+var character1 = new Player(addCharacter());
+var character2 = new Player("Jim", 3, 5, 6, 21, 15, 12);
+
+
+playerArray = [character1, character2];
+
+console.log(playerArray);
+
+console.log(character1.Attack());
+*/
+
+/*
+Monster.prototype = {
+	Attack: function () {
+		var role = randomNumber(1,20);
+		if (playerClass == "undefined") {
+			this.strength += role;
+			alert (this.name + " has " + this.strength + " for Strength \n" + this.dexterity + "		for Dexterity \n");
+		} else {
+			this.dexterity += role;
+		} 
+	};
+}
+*/
 
 
 
@@ -51,4 +99,13 @@ var Monster =
 
 
 
-console.log(document.getElementById('addMonster').addEventListener('click', Player));
+ 
+
+
+
+
+
+
+
+
+
--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-var Monster = { name: "Kelly", 
+var Monster = { name: "tryOut",
 				strength: 8, 
 				dexterity: 7, 
 				constitution: 10, 
@@ -6,10 +6,47 @@ var Monster = { name: "Kelly",
 				intelligence: 18, 
 				charisma: 15 }
 
-console.log(Monster);
+/*
+var Player1 = new Player ("Kelly", 8, 6, 5, 10, 11, 13);
+var Player2 = new Player ("Jim", 3, 5, 6, 21, 15, 12);
+
+
+playerArray = [Player1, Player2];
+
+console.log(playerArray);
+
 
 var Player = Object.create(Monster);
 Player.playerClass = "";
 
 
+function Player (name,strength,dexterity,constitution,wisdom,intelligence,charisma,playerclass) {
+	this.name = name;
+	this.strength = strength;
+	this.dexterity = dexterity;
+	this.constitution = constitution;
+	this.wisdom = wisdom;
+	this.intelligence = intelligence;
+	this.charisma = charisma;
+	if (this.playerClass) {
+		this.playerClass = playerClass;
+	} 
+
+};
+
 console.log(Player);
+
+/*
+Monster.prototype = {
+	Attack: function () {
+		var role = randomNumber(1,20),
+			this.strength += role;
+			alert (playerArray.name + " has " + playerArray.strength + " for Strength \n" + playerArray.dexterity + "		for Dexterity \n");
+		};
+	}
+
+console.log(Monster.Attack());
+
+
+console.log()
+*/
